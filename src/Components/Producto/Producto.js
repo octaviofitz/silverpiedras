f163ed2dd7daa2c8b8d3c55e06b674eee6c51ada
octaviofitz <octaviofitz@gmail.com>
2024-05-03 02:26:56 -0300

    fix producto

--- src/Components/Producto/Producto.js
+++ src/Components/Producto/Producto.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ProductoFrontal from '../IMG/mockupFrontal.png';
+import ProductoDorso from '../IMG/MockupDorso.png';
 import BannerML from '../IMG/BannerDesktop.webp';
 
 import './producto.css';
@@ -16,10 +17,20 @@ function Producto() {
                      <div className='textos'>
                         <h5 className='subtitulo'>Silver</h5>
                         <p className='descripcion'>Silver posee una gran cualidad de absorción debido a su estructura porosa y a la alta pureza del mineral. Los microporos internos, favorecen la retención e inhibición de bacterias causantes del mal olor en la bandeja sanitaria.</p>
-                        <p className='descripcion'>Disponible en 3.8KG</p>
-                        
+                        <p className='descripcion'> <strong>Disponible en 3.8KG</strong></p>
                          </div>
-        </div>
+                         </div>
+
+                <div className='container' id='Container'>
+                         <div>
+                            <img src={ProductoDorso} alt='Silver Piedras para gatos' className='img' />
+                         </div>
+
+                            <div className='textos'>
+                            <h5 className='subtitulo'>Diatomita</h5>
+                        <p className='descripcion'>Silver posee una gran cualidad de absorción debido a su estructura porosa y a la alta pureza del mineral. Los microporos internos, favorecen la retención e inhibición de bacterias causantes del mal olor en la bandeja sanitaria.</p>
+                            </div>
+                            </div>
         <img src={BannerML} className='banner'/>
         </section>
     );
