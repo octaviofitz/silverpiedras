cba558d941e45e92317da960190403c1e627c68f
Octavio <user@MacBook-Air-de-user.local>
2025-02-21 23:00:44 -0300

    distribuidores button

--- src/Components/Distribuidores/Distribuidores.js
+++ src/Components/Distribuidores/Distribuidores.js
@@ -6,10 +6,10 @@ function Distribuidores() {
         <section className='distribuidor'>
             <div className='containerTexto'>
             <h3 className='titulo'>Vender SILVER</h3>
-            <p className='texto'>¿Tenés una veterinaria o un pet shop y querés vender los productos de Vitalfun?</p>
+            <p className='texto'>¿Tenés un petshop, una veterinaria o sos distribuidor y querés vender Silver?</p>
             </div>
             <div className='containerBoton'>
-            <button className='boton'>Contactanos</button>
+            <button className='boton'><a href='https://api.whatsapp.com/send/?phone=1159300042' target='__blanck'>Contactanos</a></button>
             </div>
         </section>
     );
