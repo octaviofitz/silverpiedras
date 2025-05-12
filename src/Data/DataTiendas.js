f42fbafd6be9f980bcde5ab64ffa80f5524f5812
octaviofitz <octaviofitz@gmail.com>
2025-05-12 16:31:37 -0300

    direcciones

--- src/Data/DataTiendas.js
+++ src/Data/DataTiendas.js
@@ -2,8 +2,12 @@ const DataTiendas = [
     { id: 1, nombre: 'Familia Multiespecie', direccion: 'Quintino Bocayuva 82', contacto: '@familiamultiespeciearg', lat: -34.613201375318, lng: -58.42325081700583 },
     { id: 2, nombre: 'Animals Pet Shop', direccion: 'Av. Belgrano 1314', contacto: '1559029238', lat: -34.613351344381705, lng: -58.384824872828744 },
     { id: 3, nombre: 'Pet Shop Mosho', direccion: 'Av. Rivadavia 8841', contacto: '1158362229', lat: -34.63350441851307, lng: -58.48243474768853 },
+    { id: 4, nombre: 'Pet Shop Brenda', direccion: 'Av. Boedo 562', contacto: '@petshopbrenda', lat: -34.618965887574745, lng: -58.416492945489544 },
+    { id: 5, nombre: 'Comi Mascot', direccion: 'Colombres 193', contacto: '111556266215', lat: -34.61401781796543, lng: -58.418813956884655 },
+    { id: 6, nombre: 'Tienda para Mascotas', direccion: 'Virrey Liniers 50', contacto: '1148645049', lat: -34.61130530384428, lng: -58.4154220815306 },
+    { id: 7, nombre: 'Pet Shop Tres Cachorros', direccion: 'Av. Castañares 1115', contacto: '1151468323', lat: -34.63519671198863, lng: -58.434746147332994 },
+
   ];
   
   export default DataTiendas;
   
-
