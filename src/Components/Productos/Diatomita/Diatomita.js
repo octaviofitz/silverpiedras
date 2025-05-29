719e674b99226558cb68cb329c25ce47c5b647f5
octaviofitz <octaviofitz@gmail.com>
2025-05-29 18:09:32 -0300

    change text

--- src/Components/Productos/Diatomita/Diatomita.js
+++ src/Components/Productos/Diatomita/Diatomita.js
@@ -13,7 +13,7 @@ function Diatomita() {
                     </div>
                     <div className="data">
                         <h2 className="titulo">Diatomita</h2>
-                        <p className="texto">Nuestra Diatomita para Gatos es una alternativa natural y ultraabsorbente para el arenero de tu mascota. Gracias a su composición liviana y porosa, controla eficazmente la humedad y neutraliza los olores, manteniendo el ambiente fresco por más tiempo. Además, es biodegradable y libre de químicos, ideal para quienes buscan una opción ecológica y segura.</p>
+                        <p className="texto">Nuestra Diatomita para Gatos es una alternativa natural y ultraabsorbente para el arenero de tu mascota. Gracias a su composición liviana y porosa, controla eficazmente la humedad y neutraliza los olores, manteniendo el ambiente fresco por más tiempo.</p>
                     </div>
                 </article>
     );
